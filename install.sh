3d67fb2b7198720444f2927aa228dca0b72a4f4a
Robin Pfeiffer <contact@rbn.dev>
2021-05-28 23:50:17 +0200

    Fix ShellCheck SC2039 in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 theme=$(echo "$BASH_IT_THEME" | sed 's_/_\\/_g')
 mirage=$(echo "$PWD" | sed 's_/_\\/_g')
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
     sed -i 's/'"$theme"'/'"$mirage\\/mirage.theme.bash"'/g' ~/.bash_profile
 else
     sed -i 's/'"$theme"'/'"$mirage\\/mirage.theme.bash"'/g' ~/.bashrc
