3eaf2f0518e5a30b451658fde6932ab8f8dbd501
Robin Pfeiffer <contact@rbn.dev>
2021-09-09 14:47:57 +0200

    Update install script for macOS
    - Change sed command to be more flexible.
    - Fix sed -i error on macOS (BSD of sed doesn't accept empty -i.

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
-
-theme=$(echo "$BASH_IT_THEME" | sed 's_/_\\/_g')
 mirage=$(echo "$PWD" | sed 's_/_\\/_g')
 
 if [ "$(uname)" = "Darwin" ]; then
-    sed -i 's/'"$theme"'/'"$mirage\\/mirage.theme.bash"'/g' ~/.bash_profile
+    sed -i -e 's/BASH_IT_THEME=.*/BASH_IT_THEME='"$mirage\\/mirage.theme.bash"'/g' ~/.bash_profile
 else
-    sed -i 's/'"$theme"'/'"$mirage\\/mirage.theme.bash"'/g' ~/.bashrc
+    sed -i -e 's/BASH_IT_THEME=.*/BASH_IT_THEME='"$mirage\\/mirage.theme.bash"'/g'  ~/.bashrc
 fi
